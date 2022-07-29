4b9aac4597cd17e1389d652ec0dd0898ac30fcd2
seyxxn <lucy9480@gmail.com>
2022-07-29 15:47:44 +0900

    footer 수정

--- lecommend-ssu/src/components/Footer.jsx
+++ lecommend-ssu/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className="contact">땅벤져스</div>
         <div className="contactInfo">
           💡 SSU 2022 데이터톤<br></br>
-          20170614 IT융합학과 유승종<br></br>
+          20170614 전자정보공학부 IT융합전공 유승종<br></br>
           20192981 컴퓨터학부 류효경<br></br>
           20200048 국어국문학과 정승연<br></br>
           20201841 컴퓨터학부 박세연<br></br>
