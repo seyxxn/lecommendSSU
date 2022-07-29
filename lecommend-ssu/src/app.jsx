f4fd1d96dde25b8650be4b83454d65dcf328449b
seyxxn <lucy9480@gmail.com>
2022-07-29 17:06:35 +0900

    add predict congetstion page

--- lecommend-ssu/src/app.jsx
+++ lecommend-ssu/src/app.jsx
@@ -8,6 +8,7 @@ import Recommend from "./pages/Recommend";
 import Congestion from "./pages/Congestion";
 import RealTimeCongestion from "./pages/RealTimeCongestion";
 import DayAndMonthCongestion from "./pages/DayAndMonthCongestion";
+import PredictCongestion from "./pages/PredictCongestion";
 
 class app extends Component {
   render() {
@@ -27,6 +28,11 @@ class app extends Component {
             exact
             path="/dayandmonthcongestion"
             element={<DayAndMonthCongestion />}
+          />
+          <Route
+            exact
+            path="/predictCongestion"
+            element={<PredictCongestion />}
           />
         </Routes>
         <Footer />
--- lecommend-ssu/src/components/ButtonContainer2.jsx
+++ lecommend-ssu/src/components/ButtonContainer2.jsx
@@ -24,6 +24,15 @@ function ButtonContainer2() {
         >
           <Button2 serviceName="요일·시간별 혼잡도 확인하기"></Button2>
         </Link>
+        <Link
+          to="/predictCongestion"
+          style={{
+            textDecoration: "none",
+            cursor: "pointer",
+          }}
+        >
+          <Button2 className="" serviceName="혼잡도 예측하기"></Button2>
+        </Link>
       </div>
     </>
   );
